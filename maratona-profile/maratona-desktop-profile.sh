0fd8ed0fcfb371e7c0e904fe7db68ac9157d2bf5
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2018-07-14 19:40:41 -0300

    maratona-desktop: Adicionado script que altera metadado do icones .desktop. (#18)
    maratona-profile/maratona-desktop-profile.sh: Adicionado um script que é
    executado ao iniciar a secção de usuário. Esse script possui como objetivo
    colocar os .desktop da área de trabalho (criado pelo maratona-usuario-icpc) como
    confiavéis. Portanto, como solução, é adicionar esse script em /etc/profile.d/
    no qual possui scripts que é executado ao inicializar a secção de usuário.
    Dentro do script é dado um "gio set <icon.desktop> "metadata::trusted" yes",
    assim, ao inicializar a secção, os icones da área de trabalho ficam com os
    icones verdadeiros, descritos no seu .desktop, sem precisar marcar o icone como confiavel.
    
    debian/rules: O Script acima tem como nome maratona-desktop-profile.sh e é
    copiado para /etc/profile.d.
    
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

---
+++ maratona-profile/maratona-desktop-profile.sh
@@ -0,0 +1,10 @@
+# Percorre todos os arquivos.desktop na home do usuario de deixa
+# Os arquivos confiaveis
+if [ ! -f "$HOME/.config/desktop-trusted" ] ; then
+	for x in $HOME/Desktop/*.desktop ; do
+		[ -f $x ] && gio set $x "metadata::trusted" yes
+	done
+
+	echo "yes" > $HOME/.config/desktop-trusted
+
+fi
