8c28bd9209acc687a373a0638bcdb3bdef81d576
Wall Berg Morais <wallbergmirandamorais@gmail.com>
2018-08-15 23:12:07 -0300

    maratona-profile/maratona-desktop-profile.sh: Tirando verificação de arquivo. (#26)
    Tirando verificação se o arquivo .desktop-trusted foi criado na pasta
    ~/.config/desktop-trusted para que o script funcione com sucesso na imagem que
    sera gerada do maratona-linux. Como é um script que executa para poucos
    arquivos, somente os atalhos na área de trabalho, sua execução tem pouca
    influência na inicialização da sessão de usuário.
    
    Signed-off-by: Wall Berg Morais <wallbergmirandamorais@gmail.com>

--- maratona-profile/maratona-desktop-profile.sh
+++ maratona-profile/maratona-desktop-profile.sh
@@ -1,10 +1,10 @@
 # Percorre todos os arquivos.desktop na home do usuario de deixa
 # Os arquivos confiaveis
-if [ ! -f "$HOME/.config/desktop-trusted" ] ; then
-	for x in $HOME/Desktop/*.desktop ; do
-		[ -f $x ] && gio set $x "metadata::trusted" yes
-	done
+#if [ ! -f "$HOME/.config/desktop-trusted" ] ; then
 
-	echo "yes" > $HOME/.config/desktop-trusted
+for x in $HOME/Desktop/*.desktop ; do
+	[ -f $x ] && gio set $x "metadata::trusted" yes
+done
 
-fi
+#echo "yes" > $HOME/.config/desktop-trusted
+#fi
