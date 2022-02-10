5e9725cc687a380a689eac2b3c0c930ac78c0ca7
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-02-10 17:52:43 -0300

    Enable specifying the package's version
    Implement a function that enables the user to specify a version for the
    package to be installed. This fixes the problems with non-reproducible
    builds, as the Flatpaks were always downloaded in their newest versions
    available on Flathub.
    
    In order to specify a version, the user has to pass the OSTree commit as
    an argument. Due to Flatpak's limitations, the newest package will be
    installed first, and only then the specified version will replace it.
    
    The new function uses the older one to install the standard version
    offered by Flathub. This installation procedure, as expected from the
    old behaviour, will be attempted three times.
    
    If no version is specified, the new function will install the newest
    version in the repository. If a version is specified, the function will
    attempt to install it three times. If this installation fails, the
    program will attempt to remove the package.
    
    The OSTree commit id can be obtained with the command `flatpak
    remote-info --log ${remote_name} ${package_name}`.
    
    - create function for specifying a OSTree commit to be installed instead
      of the default one offered by the remote repository
    - create function to remove a Flatpak package
    - protect the arguments from the old function from unexpected expansions

--- flatpak-common-script/install_flatpak.sh
+++ flatpak-common-script/install_flatpak.sh
@@ -8,15 +8,70 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 try_install_flatpak() {
 	try=0
-	while [ $(flatpak list | grep $1 | wc -l) -eq 0 ] && [ $try -lt 3 ]; do
-		flatpak install flathub $1 -y
+	while [ "$(flatpak list | grep "$1" | wc -l)" -eq 0 ] && [ $try -lt 3 ]; do
+		flatpak install flathub "$1" -y
 	    try=$((try+1))
 	done
 
 	# In case of success, return 0. In case of failure, return 1.
-	if [ $(flatpak list | grep $1 | wc -l) -eq 0 ]; then
+	if [ "$(flatpak list | grep "$1" | wc -l)" -eq 0 ]; then
 		return 1
 	else
 		return 0
 	fi
-} 
+}
+
+# Tries to remove a flatpak
+# Arguments:
+# $1 - package name
+try_remove_flatpak() {
+	if flatpak remove --assumeyes "$1"; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+# Tries to install an specific version of a flatpak
+# If no version is specified, installs the ropository's default
+# Arguments:
+# $1 - package name
+# $2 - package version
+# Return codes:
+# 0 - package successfully installed
+# 1 - package could not be installed
+# 2 - failed to install the required version
+# 3 - failed to install the required version and remove the current one
+try_install_flatpak_version() {
+	local try=0
+	local -r max_tries=3
+
+	if try_install_flatpak "$1"; then
+		# no version specified
+		if [ ! "$2" ]; then
+			return 0
+		fi
+
+		while ! flatpak update --assumeyes --commit="$2" "$1" && [ "${try}" -lt "${max_tries}" ]; do
+			try=$((try+1))
+		done
+
+		local current_ver
+		current_ver=$(flatpak info --show-commit "$1")
+		local ver_status=$?
+
+		if [ "${ver_status}" -ne 0 ]; then
+			if [ "${current_ver}" == "$2" ]; then
+				return 0
+			else
+				if try_remove_flatpak "$1"; then
+					return 2
+				else
+					return 3
+				fi
+			fi
+		fi
+	else
+		return 1
+	fi
+}
